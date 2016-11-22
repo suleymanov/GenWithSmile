1bb1df072702b2a3d5d318df0bdfc4120c3a5392
Rail Suleymanov <rail.suleymanov@gmail.com>
2016-11-22 16:34:35 +0300

    client.py update

--- client.py
+++ client.py
@@ -8,7 +8,6 @@ from comb_lib_functions import get_combinations
 
 
 def main(args):
-	print('hi')
 	settings_fn = args[0]
 	beg_ind = int(args[1])
 	end_ind = int(args[2])
@@ -20,15 +19,13 @@ def main(args):
 	result_fn = settings['result_file_name']
 	with open(smiles_fn) as f:
 		mol_smiles_list = f.read().splitlines()
-	# for i in xrange(len(mol_smiles_list)):
-        # for i in xrange(3):
 	for i in xrange(beg_ind, end_ind):
 		for j in xrange(i, len(mol_smiles_list)):
 			print('i = {},\tj = {}'.format(i, j))
 			_combinations = get_combinations(
 				mol_smiles_list[i], mol_smiles_list[j], source_atom, target_atom)
 			with open(result_fn.format(i, j), 'w') as f:
-				f.write('\n'.join(_combinations))
+				f.write('\n'.join(_combinations) + '\n')
 
 
 if __name__ == '__main__':
